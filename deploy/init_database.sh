18e6ca3984c59052d6e1bd20086ba79e40a7f671
jsaffe <jorgesaffe@gmail.com>
2023-01-20 01:43:23 -0300

    gunicorn ok

--- deploy/init_database.sh
+++ deploy/init_database.sh
@@ -9,8 +9,6 @@ python manage.py makemigrations variables
 python manage.py migrate
 
 python manage.py createsuperuser
-#python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin','admin@admin.com', 'radar2017')"
 
-# dump database
-# python manage.py dumpdata --natural-foreign --natural-primary -e auth.permission -e contenttypes -e sessions -e admin -e api_radares.RadarImage --indent=4 > db_bak.json
+# dump database: python manage.py dumpdata --natural-foreign --natural-primary -e auth.permission -e contenttypes -e sessions -e admin -e api_radares.RadarImage --indent=4 > db_bak.json
 python manage.py loaddata ./deploy/init_database_data.json
